b5034e54cc042fe1a95a4666333726b51099c6a5
Julien Lamy <lamy@unistra.fr>
2022-08-20 10:37:45 +0200

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, "README.md")) as f:
 
 setup(
     name="pandoc-purl",
-    version="0.1.0",
+    version="0.2.0",
     
     description="Dynamic document generation for Pandoc in Python",
     long_description=long_description,
